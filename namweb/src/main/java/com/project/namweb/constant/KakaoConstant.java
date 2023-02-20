f69c075be3ca930092348e9c9a64d63f68cd827b
cdnwell <cdnwellhk@gmail.com>
2023-02-20 20:43:39 +0900

    test : hide check

--- namweb/src/main/java/com/project/namweb/constant/KakaoConstant.java
+++ namweb/src/main/java/com/project/namweb/constant/KakaoConstant.java
@@ -1,5 +1,5 @@
 package com.project.namweb.constant;
 
 public interface KakaoConstant {
-	public String clientId = "temporary code";
+	public String clientId = "f35af5c1aa31edce035b8a9953ba2858";
 }
