084444a33a5c58648318da6266257a4a8488094e
cdnwell <cdnwellhk@gmail.com>
2023-11-07 22:04:04 +0900

    - add 1. 회원가입 close 버튼 추가 2. 아이디, 비밀번호 유효성 검사 추가

--- chatapp/src/main/chatapp/src/domain/login/register/RegisterBox.tsx
+++ chatapp/src/main/chatapp/src/domain/login/register/RegisterBox.tsx
@@ -3,34 +3,86 @@ import classes from "./RegisterBox.module.scss";
 import PasswordField from "./password/PasswordField";
 import RegProfile from "./profile/RegProfile";
 
+import { AiFillCloseCircle } from "react-icons/ai";
+import {useNavigate} from "react-router-dom";
+import {ChangeEvent, useEffect, useState} from "react";
+
+const invalidIdComment = "아이디는 숫자 혹은 영어로 이루어 져야합니다.";
+const invalidPwComment = "비밀번호가 같지않습니다.";
+
 const RegisterBox = () => {
+    const [id, setId] = useState<string>("");
+    const [isInvalidId, setIsInvalidId] = useState<boolean>(false);
+    const [firstPw, setFirstPw] = useState<string>("");
+    const [secondPw, setSecondPw] = useState<string>("");
+    const [isInvalidPw, setIsInvalidPw] = useState<boolean>(false);
+
+    const navigate = useNavigate();
+
+    const onCloseBtnHandler = () => {
+        navigate(-1);
+    }
+
+    const onIdInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const id = event.target.value;
+        setId(id);
+    }
+
+    const onPwInputHandler = (event: ChangeEvent<HTMLInputElement>, pwSetter: React.Dispatch<React.SetStateAction<string>>) => {
+        const pw = event.target.value;
+        pwSetter(pw);
+    }
+
+    useEffect(() => {
+        console.log('first pw :', firstPw);
+        console.log('second pw :', secondPw);
+    }, [firstPw, secondPw]);
+
+    const onLoginSubmit = () => {
+        setIsInvalidId(!idValidator(id));
+        setIsInvalidPw(!pwValidator(firstPw, secondPw));
+    }
+
+    const idValidator = (id: string): boolean => {
+        const idReg = /^[a-zA-Z0-9]+$/;
+        return idReg.test(id);
+    }
+
+    const pwValidator = (firstPw: string, secondPw: string): boolean => {
+        return firstPw === secondPw;
+    }
+
+
     return (
         <div className={classes.register_div}>
+            <AiFillCloseCircle className={classes.reg_close_btn} onClick={onCloseBtnHandler} />
             <RegProfile />
             <div className={classes.reg_id_div}>
                 <p className={classes.id_p}>
-                    이메일
+                    아이디
                 </p>
-                <input type="text" placeholder="example@mail.com" />
+                <input type="text" placeholder="아이디를 입력해주세요." onChange={onIdInputHandler} maxLength={15} />
+                <span className={classes.id_span}>{isInvalidId ? invalidIdComment : ""}</span>
             </div>
             <div className={classes.reg_pw_div}>
                 <p className={classes.pw_p}>
                     비밀번호
                 </p>
-                <PasswordField placeholder="∗∗∗∗∗∗" />
+                <PasswordField placeholder="∗∗∗∗∗∗" pwHandler={(event) => onPwInputHandler(event, setFirstPw)} />
+                <span className={classes.pw_span}>{isInvalidPw ? invalidPwComment : ""}</span>
             </div>
             <div className={classes.re_pw_div}>
                 <p className={classes.pw_p}>
                     비밀번호 재입력
                 </p>
-                <PasswordField placeholder="∗∗∗∗∗∗" />
+                <PasswordField placeholder="∗∗∗∗∗∗" pwHandler={(event) => onPwInputHandler(event, setSecondPw)} />
             </div>
             <div className={classes.reg_name_div}>
                 <p className={classes.name_p}>닉네임</p>
-                <input type="text" placeholder="nick" />
+                <input type="text" placeholder="닉네임을 입력해주세요." />
             </div>
             <div className={classes.reg_permit_btn}>
-                <button className={classes.reg_btn}>제출</button>
+                <button className={classes.reg_btn} onClick={onLoginSubmit}>제출</button>
             </div>
         </div>
     )
--- chatapp/src/main/chatapp/src/domain/login/register/password/PasswordField.tsx
+++ chatapp/src/main/chatapp/src/domain/login/register/password/PasswordField.tsx
@@ -1,23 +1,25 @@
 import classes from "./PasswordField.module.scss";
 
-import { useState } from "react";
+import {ChangeEvent, useState} from "react";
 
 import { AiOutlineEye } from "react-icons/ai";
 
 interface PasswordFieldProps {
-    placeholder : string;
+    placeholder: string;
+    pwHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const PasswordField : React.FC<PasswordFieldProps>  = ({ placeholder }) => {
+const PasswordField : React.FC<PasswordFieldProps>  = ({ placeholder, pwHandler }) => {
     const [isPasswordShown, setPasswordShown] = useState<boolean>(false);
-    
+
     const togglePasswordVisiblity = () => {
         setPasswordShown(!isPasswordShown);
     }
 
+
     return (
     <div className={classes.pw_wrapper}>
-        <input type={isPasswordShown ? "text" : "password"} placeholder={placeholder} />
+        <input type={isPasswordShown ? "text" : "password"} placeholder={placeholder} onChange={pwHandler} maxLength={15} />
         <AiOutlineEye className={classes.pw_eye_icon} onClick={togglePasswordVisiblity} />
     </div>
     );
